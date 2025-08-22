428be6fdfa4fb10b5bd965e57f5a9bcd689b45ab
N-Reed1 <nickereed1@gmail.com>
2025-08-22 17:25:58 -0400

    changed the color of the title bar

--- src/main/index.js
+++ src/main/index.js
@@ -10,6 +10,7 @@ function createWindow() {
     height: 670,
     show: false,
     autoHideMenuBar: true,
+    backgroundColor: '#27272a',
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
